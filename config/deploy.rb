6279f6549a33d81be5574b87a040df40179ac849
Marten Veldthuis <marten@veldthuis.com>
2009-10-28 19:44:37 +0100

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,84 @@
+set :scm,         :git
+set :repository,  "git@github.com:marten/ijbel.git"
+set :branch,      "origin/master"
+set :migrate_target, :current
+set :application, "ijbel"
+set :deploy_to,   "/home/marten/web/ijbel.org"
+set :user,        "marten"
+
+role :web, "robinson.veldthuis.com"                          # Your HTTP server, Apache/etc
+role :app, "robinson.veldthuis.com"                          # This may be the same as your `Web` server
+role :db,  "robinson.veldthuis.com", :primary => true        # This is where Rails migrations will run
+
+set(:latest_release)  { fetch(:current_path) }
+set(:release_path)    { fetch(:current_path) }
+set(:current_release) { fetch(:current_path) }
+
+set(:current_revision)  { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:latest_revision)   { capture("cd #{current_path}; git rev-parse --short HEAD").strip }
+set(:previous_revision) { capture("cd #{current_path}; git rev-parse --short HEAD@{1}").strip }
+
+namespace :deploy do
+
+  desc "Deploy the MFer"
+  task :default do
+    update
+    restart
+  end
+
+  desc "Setup a GitHub-style deployment."
+  task :setup, :except => { :no_release => true } do
+    dirs = [deploy_to, shared_path]
+    dirs += shared_children.map { |d| File.join(shared_path, d) }
+    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+    run "git clone #{repository} #{current_path}"
+  end
+  
+  task :update do
+    transaction do
+      update_code
+    end
+  end
+
+  desc "Update the deployed code."
+  task :update_code, :except => { :no_release => true } do
+    run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
+    finalize_update
+  end
+  
+  desc "Update the database (overwritten to avoid symlink)"
+  task :migrations do
+    update_code
+    migrate
+    restart
+  end
+
+  namespace :rollback do
+    desc "Moves the repo back to the previous version of HEAD"
+    task :repo, :except => { :no_release => true } do
+      set :branch, "HEAD@{1}"
+      deploy.default
+    end
+    
+    desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
+    task :cleanup, :except => { :no_release => true } do
+      run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
+    end
+    
+    desc "Rolls back to the previously deployed version."
+    task :default do
+      rollback.repo
+      rollback.cleanup
+    end
+  end
+
+  task :start do
+  end
+
+  task :stop do
+  end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
